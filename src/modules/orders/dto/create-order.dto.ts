b0b297460faf33e79eb4afb955ff6d2d0259fa5b
Agusmalugani <agusmalugani97@gmail.com>
2024-12-08 11:22:32 -0300

    ultimas modificaciones

--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -10,7 +10,7 @@ export class CreateOrderDto {
     idUser:string
 
     /** Debe ser un array y contener almenos un producto
-     * @example products:[{id:8f2b02d6-q548-6a1s-6w4f-77q868b5q64e}]
+     * @example [{id:8f2b02d6-q548-6a1s-6w4f-77q868b5q64e}]
      */
     @IsArray()
     @ArrayMinSize(1)
